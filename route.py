07f4835470c1c66f15f85468da9cbe7dc310c7ee
Mu-Wei Chung <raymondc0302@gmail.com>
2022-12-16 03:13:35 +0800

    prettify and don't redirect if no results is found

--- route.py
+++ route.py
@@ -23,21 +23,31 @@ def result():
             # return redirect(url_for("index"))
 
         units = dict()
+        not_found_any_unit_url = True # whether any unit outline url is found
         # check if the unit code is valid
         for unit_code in unit_codes.split():
             if not args.valid_unit_code(unit_code):
-                flash(unit_code.upper() + " is not in correct format! A valid unit code should be 4 letters followed by 4 digits.")
+                flash("Unit \"" + unit_code.upper() + "\" is not in correct format! A valid unit code should be 4 letters followed by 4 digits.")
 
             else:
                 result = crawl.get_unit_outline_url(unit_code, year, False)
                 if result == None:
-                    flash(unit_code.upper() + " was not found!")
+                    flash("Unit \"" + unit_code.upper() + "\" was not found!")
                 elif len(result) == 0:
-                    flash(unit_code.upper() + f" outline in  {year} was not found!")
+                    flash("Unit code \"" + unit_code.upper() + f"\" outline in  {year} was not found!")
                 else:
-                    units[unit_code] = result
+                    units[unit_code.upper()] = result
+                    not_found_any_unit_url = False
 
-        return render_template("result.html", unit_codes=unit_codes, units=units)
+        # if unit_codes is empty, return to index page
+        if not_found_any_unit_url:
+            return render_template("index.html")
+        else:
+            return render_template("result.html", unit_codes=unit_codes, units=units)
     
     else:
-        return render_template("result.html")
+        return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
